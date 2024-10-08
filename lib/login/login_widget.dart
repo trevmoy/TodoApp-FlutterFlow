0ca75cd2bccbc8f2e7e36b4176f873664a628fc2
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-08 00:22:19 +0000

    Updating to latest FlutterFlow output.

--- lib/login/login_widget.dart
+++ lib/login/login_widget.dart
@@ -1,4 +1,5 @@
 import '/auth/firebase_auth/auth_util.dart';
+import '/backend/api_requests/api_calls.dart';
 import '/backend/backend.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
@@ -785,6 +786,10 @@ class _LoginWidgetState extends State<LoginWidget>
                                     ),
                                     createdTime: getCurrentTimestamp,
                                   ));
+
+                              await SendRegistrationEmailCall.call(
+                                to: _model.signupEmailTextController.text,
+                              );
 
                               context.goNamedAuth(
                                   'onboarding', context.mounted);
